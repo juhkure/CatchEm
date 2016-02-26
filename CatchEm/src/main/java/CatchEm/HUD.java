4889c982722f542e33af0a22a11618424cc8753e
Johannes Pyykönen <johannes.pyykonen@helsinki.fi>
2016-02-26 03:13:34 +0200

    paljon päivityksiä

---
+++ CatchEm/src/main/java/CatchEm/HUD.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CatchEm;
+
+import static CatchEm.Game.height;
+import static CatchEm.Game.width;
+import java.awt.Color;
+import java.awt.Graphics;
+import static java.lang.Math.random;
+import static java.lang.StrictMath.random;
+import java.text.DecimalFormat;
+import java.util.Random;
+
+/**
+ *
+ * @author Johannes
+ */
+public class HUD {
+
+    private int ticksInSecond;
+    private double time;
+    private double timeInOneTick;
+    private DecimalFormat df;
+    private Handler handler;
+    private Random random;
+
+    public HUD(Handler handler) {
+        this.random = new Random();
+        this.handler = handler;
+        this.ticksInSecond = 600;
+        this.time = 10.0;
+        timeInOneTick = (double) time / ticksInSecond;
+    }
+
+    public void tick() {
+        time -= timeInOneTick;
+
+        if (time <= 0.0) {
+            time = 0.0;
+
+            for (int i = 0; i < handler.object.size(); i++) {
+                GameObject tempObject = handler.object.get(i);
+
+                if (tempObject.getId() == ID.Target) {
+                    handler.removeObject(tempObject);
+                }
+            }
+        }
+    }
+
+    public void render(Graphics g) {
+
+        String timeInString = String.format("%.1f", time);
+        g.setColor(Color.white);
+        g.drawString(timeInString, 15, 15);
+        
+        
+
+        if (time <= 0.0) {
+            g.setColor(Color.red);
+            g.fillRect(258, 149, 65, 15);
+
+            g.setColor(Color.black);
+            g.drawString("Game over", 260, 160);
+
+            String scoreInString = Integer.toString(handler.player1Score());
+
+            g.setColor(Color.white);
+            g.drawString("Your score: " + scoreInString, 255, 185);
+
+            g.setColor(Color.green);
+            g.drawString("Press esc to close,", 255, 210);
+
+            g.drawString("or press enter to try again", 255, 225);
+
+        }
+    }
+
+    public void addTime() {
+        time += 0.75;
+    }
+
+    public void restart() {
+        for (int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+            
+            if (tempObject.getId() == ID.Target) {
+                handler.removeObject(tempObject);
+            }
+        }
+        time = 10.0;
+        handler.addObject(new Target(random.nextInt(width - 39), random.nextInt(height - 60), ID.Target));
+        
+        
+    }
+}
---
+++ CatchEm/src/main/java/CatchEm/KeyInput.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CatchEm;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+/**
+ *
+ * @author Johannes
+ */
+public class KeyInput extends KeyAdapter {
+//    W: 87    S: 83   A: 65   D: 68  SPACE: 32  
+    
+    private Handler handler;
+    private Game game;
+    private HUD hud;
+    private int speedBoost;
+    private int speed;
+    
+    
+    public KeyInput(Handler handler, HUD hud, Game game){
+        this.handler = handler;
+        this.game = game;
+        this.hud = hud;
+        speedBoost = 3;
+        speed = 4;
+    }
+    
+    public void keyPressed(KeyEvent e){
+        int key = e.getKeyCode();
+        
+        for (int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+            
+            if(tempObject.getId() == ID.Player){
+                
+                if(key == KeyEvent.VK_UP){ tempObject.setVelY(-4); }
+                if(key == KeyEvent.VK_DOWN){ tempObject.setVelY(4); }
+                if(key == KeyEvent.VK_LEFT){ tempObject.setVelX(-4); }
+                if(key == KeyEvent.VK_RIGHT){ tempObject.setVelX(4); }
+            }
+            
+            if(tempObject.getId() == ID.Player2){
+                
+                if(key == KeyEvent.VK_W){ tempObject.setVelY(-4);}
+                if(key == KeyEvent.VK_S){ tempObject.setVelY(4);}
+                if(key == KeyEvent.VK_A){ tempObject.setVelX(-4);}
+                if(key == KeyEvent.VK_D){ tempObject.setVelX(4);}
+                
+            }
+        }
+        
+        if(key == KeyEvent.VK_ESCAPE){
+            System.exit(0);
+        }
+        
+        if(key == KeyEvent.VK_ENTER){
+            hud.restart();
+            
+            for (int i = 0; i < handler.object.size(); i++) {
+                GameObject tempObject = handler.object.get(i);
+                
+                tempObject.resetScore();
+                tempObject.resetStamina();
+                
+            }
+        }
+    }
+    
+    public void keyReleased(KeyEvent e){
+        int key = e.getKeyCode();
+        
+        for (int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+            
+            if(tempObject.getId() == ID.Player){
+                
+                if(key == KeyEvent.VK_UP){ tempObject.setVelY(0); }
+                if(key == KeyEvent.VK_DOWN){ tempObject.setVelY(0); }
+                if(key == KeyEvent.VK_LEFT){ tempObject.setVelX(0); }
+                if(key == KeyEvent.VK_RIGHT){ tempObject.setVelX(0); }
+            }
+            
+            if(tempObject.getId() == ID.Player2){
+                
+                if(key == KeyEvent.VK_W){ tempObject.setVelY(0);}
+                if(key == KeyEvent.VK_S){ tempObject.setVelY(0);}
+                if(key == KeyEvent.VK_A){ tempObject.setVelX(0);}
+                if(key == KeyEvent.VK_D){ tempObject.setVelX(0);}
+                
+            }
+        }
+    }
+}
